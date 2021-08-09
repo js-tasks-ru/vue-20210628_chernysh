85ac818dfc0511c991653f5b0a81d5ecca423c17
chernysheva_ia <iachernysheva@activebc.ru>
2021-08-09 18:32:14 +0300

    #02 - MeetupInfo

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -2,20 +2,39 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupInfo',
-
+  props: {
+    organizer: {
+      type: String,
+    },
+    place: {
+      type: String,
+    },
+    date: {
+      type: Number,
+    }
+  },
+  computed: {
+    date_formating() {
+      return new Date(this.date).toLocaleString('ru', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+  },
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="2020-01-01">{{ date_formating }}</time>
       </li>
     </ul>`,
 });
